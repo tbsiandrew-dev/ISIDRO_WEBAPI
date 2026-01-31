c74f165d25378bd37fba13f495201f5a3f64326b
tbsiAndrew <your-email@domain.com>
2026-01-31 16:06:20 +0800

    UPDATED PORT IN MAIN.PY

--- main.py
+++ main.py
@@ -80,4 +80,6 @@ async def database_info(db: AsyncSession = Depends(get_db)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    import os
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
